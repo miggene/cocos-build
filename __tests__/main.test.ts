1e019a3b54675d72edf3de57c22f72fa9efd07c6
zpf <zpf518518@gmail.com>
2021-09-13 20:50:53 +0800

    chore: test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: zhupengfei
+ * @Date: 2021-09-08 15:07:05
+ * @LastEditTime: 2021-09-13 20:36:31
+ * @LastEditors: zhupengfei
+ * @Description:
+ * @FilePath: /cocos-build/__tests__/main.test.ts
+ */
 import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
@@ -9,17 +17,22 @@ test('throws invalid number', async () => {
   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+// test('wait 500 ms', async () => {
+//   const start = new Date()
+//   await wait(500)
+//   const end = new Date()
+//   var delta = Math.abs(end.getTime() - start.getTime())
+//   expect(delta).toBeGreaterThan(450)
+// })
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  // process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_COCOS_DOWNLOAD_URL'] =
+    'https://creator-api.cocos.com/api/cocoshub/editor_version_list?lang=zh'
+  process.env['INPUT_COCOS_VERSION'] = ''
+  process.env['INPUT_COCOS_TYPE'] = '2d'
+  process.env['INPUT_PROJECT_PATH'] = './'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,57 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /*
  * @Author: zhupengfei
  * @Date: 2021-09-08 15:07:05
- * @LastEditTime: 2021-09-13 18:14:33
+ * @LastEditTime: 2021-09-13 20:50:20
  * @LastEditors: zhupengfei
  * @Description:
  * @FilePath: /cocos-build/src/main.ts
  */
 import * as core from '@actions/core'
-import axios from 'axios'
-import {exec} from '@actions/exec'
+// import axios from 'axios'
+// import {exec} from '@actions/exec'
+import {downloadTool} from '@actions/tool-cache'
 
 // import {wait} from './wait'
 
-type CCDownloadType = {version: string; darwin: string; win32: string}
+// type CCDownloadType = {version: string; darwin: string; win32: string}
 
 async function run(): Promise<void> {
   try {
-    const cocosVersion = core.getInput('cocos-version')
-    core.debug(`cocos-version->: ${cocosVersion}`)
-    console.log('cocosVersion :>> ', cocosVersion)
-
-    const projectPath = core.getInput('project-path')
-    core.debug(`project-path->: ${projectPath}`)
-    console.log('projectPath :>> ', projectPath)
-
-    try {
-      const response = await axios.get(
-        'https://creator-api.cocos.com/api/cocoshub/editor_version_list?lang=zh'
-      )
-      const {data} = response.data
-      const ccDownloadItem = (data['2d'] as CCDownloadType[]).find(value => {
-        return value.version === cocosVersion
-      })
-      const dlUrl = ccDownloadItem?.darwin
-      console.log('dlUrl :>> ', dlUrl)
-      await exec(`echo 'download start'`)
-      await exec(`wget ${dlUrl} -O CocosCreator_V${cocosVersion}.zip`)
-      await exec(`echo 'download end'`)
-      await exec(`echo 'unzip start'`)
-      await exec(`unzip CocosCreator_V${cocosVersion}.zip`)
-      await exec(`echo 'unzip end'`)
-      await exec(`ls -al`)
-      await exec(`echo 'open app start'`)
-      await exec(`open ./CocosCreator.app`)
-      await exec(`echo 'open app end'`)
-      await exec(
-        `./CocosCreator.app/Contents/MacOS/CocosCreator --path ${projectPath} --build`
-      )
-
-      // shell.exec(`wget ${dlUrl} -O CocosCreator_V${'2.4.2'}.zip`)
-      // shell.exec(`unzip CocosCreator_V${'2.4.2'}.zip`)
-      // shell.exec(`open CocosCreator.app`)
-      // shell.exec(
-      //   `./CocosCreator.app/Contents/MacOS/CocosCreator --path ./ --build`
-      // )
-    } catch (error) {
-      core.error(error as string)
-    }
-
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
-    // core.setOutput('time', new Date().toTimeString())
+    const downloadUrls = core.getInput('cocos_download_urls')
+    // const cocosVersion = core.getInput('cocos_version')
+    // const cocosType = core.getInput('cocos_type')
+    // const projectPath = core.getInput('project_path')
+    const resp = await downloadTool(downloadUrls)
+    console.log('resp :>> ', resp)
+    return
+    // try {
+    //   const {data} = await (await axios.get(downloadUrls)).data
+    //   const urlList = data[cocosType] as CCDownloadType[]
+    //   const ccDownloadItem =
+    //     cocosVersion === ''
+    //       ? urlList[0]
+    //       : urlList.find(value => {
+    //           return value.version === cocosVersion
+    //         })
+    //   const dlUrl = ccDownloadItem?.darwin
+    //   await exec(`echo 'download start'`)
+    //   await exec(`wget ${dlUrl} -O CocosCreator_V${cocosVersion}.zip`)
+    //   await exec(`echo 'download end'`)
+    //   await exec(`echo 'unzip start'`)
+    //   await exec(`unzip CocosCreator_V${cocosVersion}.zip`)
+    //   await exec(`echo 'unzip end'`)
+    //   await exec(`ls -al`)
+    //   await exec(`echo 'open app start'`)
+    //   await exec(`open ./CocosCreator.app`)
+    //   await exec(`echo 'open app end'`)
+    //   await exec(
+    //     `./CocosCreator.app/Contents/MacOS/CocosCreator --path ${projectPath} --build`
+    //   )
+    // } catch (error) {
+    //   core.error(error as string)
+    // }
   } catch (error: any) {
     core.setFailed(error.message)
   }
