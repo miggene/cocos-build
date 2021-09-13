0479c42c1db65563ae01479b0ac36029aa668d28
zpf <zpf518518@gmail.com>
2021-09-13 13:29:36 +0800

    chore: test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 /*
  * @Author: zhupengfei
  * @Date: 2021-09-08 15:07:05
- * @LastEditTime: 2021-09-08 15:31:42
+ * @LastEditTime: 2021-09-13 13:28:57
  * @LastEditors: zhupengfei
  * @Description:
  * @FilePath: /cocos-build/src/main.ts
  */
 import * as core from '@actions/core'
+import axios from 'axios'
 // import {wait} from './wait'
 
 async function run(): Promise<void> {
@@ -16,6 +17,11 @@ async function run(): Promise<void> {
 
     const projectPath = core.getInput('project-path')
     core.debug(`project-path: ${projectPath}`)
+
+    const urls = await axios.get(
+      'https://creator-api.cocos.com/api/cocoshub/editor_version_list?lang=zh'
+    )
+    core.debug(`urls: ${urls}`)
     // const ms: string = core.getInput('milliseconds')
     // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // core.debug(new Date().toTimeString())
