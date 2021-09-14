0bc0a7021e1bbf9c5e608dcb0118e9c6bc57550e
zpf <zpf518518@gmail.com>
2021-09-14 11:54:56 +0800

    test

--- dist/index.js
+++ dist/index.js
@@ -43,7 +43,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 /*
  * @Author: zhupengfei
  * @Date: 2021-09-08 15:07:05
- * @LastEditTime: 2021-09-14 11:48:15
+ * @LastEditTime: 2021-09-14 11:53:40
  * @LastEditors: zhupengfei
  * @Description:
  * @FilePath: /cocos-build/src/main.ts
@@ -56,10 +56,9 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const downloadUrls = core.getInput('cocos_download_url');
-            console.log('downloadUrl :>> ', downloadUrls);
             const cocosVersion = core.getInput('cocos_version');
             const cocosType = core.getInput('cocos_type');
-            // const projectPath = core.getInput('project_path')
+            const projectPath = core.getInput('project_path');
             try {
                 const { data } = yield (yield axios_1.default.get(downloadUrls)).data;
                 console.log('data :>> ', data);
@@ -69,40 +68,14 @@ function run() {
                     : urlList.find(value => {
                         return value.version === cocosVersion;
                     });
-                console.log('version :>> ', version);
-                console.log('darwin :>> ', darwin);
-                // cocosVersion =
-                //   cocosVersion === ''
-                //     ? urlList[0].version
-                //     : urlList.find(value => {
-                //         return value.version === cocosVersion
-                //       })!.version
-                // const ccDownloadItem =
-                //   cocosVersion === ''
-                //     ? urlList[0]
-                //     : urlList.find(value => {
-                //         return value.version === cocosVersion
-                //       })
-                // const dlUrl = ccDownloadItem!.darwin
-                console.log('download start');
                 const ccZipPath = yield tool_cache_1.downloadTool(darwin, `CocosCreator_V${version}.zip`);
-                yield exec_1.exec(`ls -al`);
-                console.log('download end: ', ccZipPath);
                 yield tool_cache_1.extractZip(`${ccZipPath}`, './');
-                // await exec(`wget ${dlUrl} -O CocosCreator_V${cocosVersion}.zip`)
-                // await exec(`echo 'download end'`)
-                // await exec(`echo 'unzip start'`)
-                // await exec(`unzip CocosCreator_V${cocosVersion}.zip`)
-                // await exec(`echo 'unzip end'`)
-                yield exec_1.exec(`ls -al`);
-                // await exec(`echo 'open app start'`)
-                // await exec(`open ./CocosCreator.app`)
+                yield exec_1.exec(`open ./CocosCreator.app`);
                 // await exec(`echo 'open app end'`)
-                // await exec(
-                //   `./CocosCreator.app/Contents/MacOS/CocosCreator --path ${projectPath} --build`
-                // )
+                yield exec_1.exec(`./CocosCreator.app/Contents/MacOS/CocosCreator --path ${projectPath} --build`);
             }
             catch (error) {
+                console.log('error :>> ', error);
                 core.error(error);
             }
         }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 /*
  * @Author: zhupengfei
  * @Date: 2021-09-08 15:07:05
- * @LastEditTime: 2021-09-14 11:48:15
+ * @LastEditTime: 2021-09-14 11:53:40
  * @LastEditors: zhupengfei
  * @Description:
  * @FilePath: /cocos-build/src/main.ts
@@ -20,10 +20,9 @@ type CCDownloadType = {version: string; darwin: string; win32: string}
 async function run(): Promise<void> {
   try {
     const downloadUrls = core.getInput('cocos_download_url')
-    console.log('downloadUrl :>> ', downloadUrls)
     const cocosVersion = core.getInput('cocos_version')
     const cocosType = core.getInput('cocos_type')
-    // const projectPath = core.getInput('project_path')
+    const projectPath = core.getInput('project_path')
     try {
       const {data} = await (await axios.get(downloadUrls)).data
       console.log('data :>> ', data)
@@ -34,43 +33,18 @@ async function run(): Promise<void> {
           : urlList.find(value => {
               return value.version === cocosVersion
             })!
-      console.log('version :>> ', version)
-      console.log('darwin :>> ', darwin)
-      // cocosVersion =
-      //   cocosVersion === ''
-      //     ? urlList[0].version
-      //     : urlList.find(value => {
-      //         return value.version === cocosVersion
-      //       })!.version
-      // const ccDownloadItem =
-      //   cocosVersion === ''
-      //     ? urlList[0]
-      //     : urlList.find(value => {
-      //         return value.version === cocosVersion
-      //       })
-      // const dlUrl = ccDownloadItem!.darwin
-      console.log('download start')
       const ccZipPath = await downloadTool(
         darwin,
         `CocosCreator_V${version}.zip`
       )
-      await exec(`ls -al`)
-      console.log('download end: ', ccZipPath)
       await extractZip(`${ccZipPath}`, './')
-
-      // await exec(`wget ${dlUrl} -O CocosCreator_V${cocosVersion}.zip`)
-      // await exec(`echo 'download end'`)
-      // await exec(`echo 'unzip start'`)
-      // await exec(`unzip CocosCreator_V${cocosVersion}.zip`)
-      // await exec(`echo 'unzip end'`)
-      await exec(`ls -al`)
-      // await exec(`echo 'open app start'`)
-      // await exec(`open ./CocosCreator.app`)
+      await exec(`open ./CocosCreator.app`)
       // await exec(`echo 'open app end'`)
-      // await exec(
-      //   `./CocosCreator.app/Contents/MacOS/CocosCreator --path ${projectPath} --build`
-      // )
+      await exec(
+        `./CocosCreator.app/Contents/MacOS/CocosCreator --path ${projectPath} --build`
+      )
     } catch (error) {
+      console.log('error :>> ', error)
       core.error(error as string)
     }
   } catch (error: any) {
